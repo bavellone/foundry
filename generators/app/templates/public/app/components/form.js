98284878f24f424affeef1f3980a29c4305c6e75
Ben Avellone <benjamin.avellone@gmail.com>
2016-05-29 05:47:15 -0500

    Updates to Form HOC.

--- generators/app/templates/public/app/components/form.js
+++ generators/app/templates/public/app/components/form.js
@@ -6,56 +6,17 @@ import validate from '../../../../common/validate';
 import q from 'q';
 import _ from 'lodash';
 
-export default function Form(Component, constraints) {
+export default function Form(Component, constraints, formOps) {
 	return class Form extends React.Component {
 		constructor(props) {
 			super(props)
 		}
 
 		state = {
+			formData: {},
 			err: {
 				fields: [],
 				messages: []
-			}
-		};
-		
-		// TODO - let _updateForm update the formData prop
-		_updateState = (child, key, checkbox = false) => {
-			return (event) => {
-				const val = (event.target ? (checkbox ? event.target.checked : event.target.value) : event);
-
-				child.setState(_.merge({}, child.state, {
-					formData: {
-						[key]: val
-					}
-				}));
-
-				this.setState({
-					err: {
-						fields: _.filter(this.state.err.fields, field => field != key),
-						messages: this.state.err.messages
-					}
-				})
-			}
-		};
-		
-		
-		_updateForm = (child, key, checkbox = false) => {
-			return (event) => {
-				const val = (event.target ? (checkbox ? event.target.checked : event.target.value) : event);
-
-				child.setState(_.merge({}, child.state, {
-					formData: {
-						[key]: val
-					}
-				}));
-
-				this.setState({
-					err: {
-						fields: _.filter(this.state.err.fields, field => field != key),
-						messages: this.state.err.messages
-					}
-				})
 			}
 		};
 
@@ -74,25 +35,54 @@ export default function Form(Component, constraints) {
 				return q.resolve(data);
 		};
 
-		_submit = (child, data) => {
-			return Form._validate(data || child.state.formData, constraints)
+		_updateForm = (key, ops) => {
+			ops = _.merge({
+				checkbox: false
+			}, ops);
+
+			return (event) => {
+				const val = (event.target ? (ops.checkbox ? event.target.checked : event.target.value) : event);
+
+				this.setState({
+					...this.state,
+					formData: {
+						[key]: val
+					}
+				});
+
+				if (formOps.realtime)
+					this._submit();
+				else
+					this.setState({
+						...this.state,
+						err: {
+							fields: _.filter(this.state.err.fields, field => field != key),
+							messages: this.state.err.messages
+						}
+					})
+			}
+		};
+
+		_submit = (data) => {
+			return Form._validate(data || this.state.formData, constraints)
 				.then(this._onValidate, this._onValidateErr);
 		};
-		
+
 		_onValidate = (data) => {
 			this.setState({
+				...this.state,
 				err: {
 					fields: [],
 					messages: []
 				}
 			});
 			return data;
-		}
-		
+		};
+
 		_onValidateErr = (err) => {
-			this.setState({err});
+			this.setState({...this.state, err});
 			return q.reject(err);
-		}
+		};
 
 		_hasError = (field) => {
 			return _.includes(this.state.err.fields, field)
@@ -102,8 +92,9 @@ export default function Form(Component, constraints) {
 			return this.state.err.messages;
 		};
 
-		_setError = function (err) {
+		_setError = (err) => {
 			this.setState({
+				...this.state,
 				err: {
 					fields: this.state.fields,
 					messages: this.state.err.messages.concat(err)
@@ -113,6 +104,7 @@ export default function Form(Component, constraints) {
 
 		_clearError = (err) => {
 			this.setState({
+				...this.state,
 				err: {
 					fields: this.state.fields,
 					messages: _.remove(this.state.messages, msg => msg == err)
@@ -122,13 +114,15 @@ export default function Form(Component, constraints) {
 
 		render() {
 			return (<Component
-				updateState={this._updateState}
-				validate={this._validate}
-				submit={this._submit}
-				hasError={this._hasError}
-				errors={this._getErrors()}
-				setError={::this._setError}
-				clearError={this._clearError}
+				form={{
+					update: this._updateForm,
+					validate: Form._validate,
+					submit: this._submit,
+					hasError: this._hasError,
+					errors: this._getErrors,
+					setError: this._setError,
+					clearError: this._clearError
+				}}
 				{...this.props}
 			/>)
 		}

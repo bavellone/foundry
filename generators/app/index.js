fc9e6581cb5fe64787a5eb32d96a4e9e20d180b1
Ben Avellone <benjamin.avellone@gmail.com>
2016-06-04 16:33:05 -0500

    Small typo

--- generators/app/index.js
+++ generators/app/index.js
@@ -63,5 +63,5 @@ Generator.prototype.install = {
 };
 
 Generator.prototype.end = function () {
-	console.log(chalk.green('Almost done! Run "npm build:dev" to complete installation'));
+	console.log(chalk.green('Almost done! Run "npm run build:dev" to complete installation'));
 };
